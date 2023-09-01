51a727115b2e37e1de80660838280a7ec63bb364
AshimBarman <ashlabbarman@gmail.com>
2023-09-01 21:26:32 +0530

    serving html file

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const http = require('http');
 const PORT = 3000;
+const fs = require('fs');//use to reading or writting in file
+
 
 function requestHandler(req, res) {
     console.log("hello jee kaise ho saare");
     res.writeHead(200,{'context-type': 'text/html'});
-    res.end('<h1>Got it!</h1>');
+    fs.readFile('index.html', function (err,data) {
+        if(err) {
+            console.log(err);
+            return res.end('<h1>error<h1>');
+        }
+        return res.end(data);
+    });
 }
 const server = http.createServer(requestHandler);
 
