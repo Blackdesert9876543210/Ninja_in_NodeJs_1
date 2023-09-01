d5f4695ac9b3acbc06510c7d065d87a6d923bd6e
AshimBarman <ashlabbarman@gmail.com>
2023-09-01 20:56:25 +0530

    Serving something while starting the server

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const PORT = 3000;
 
 function requestHandler(req, res) {
     console.log("hello jee kaise ho saare");
+    res.end('hello I am a Page!');
 }
 const server = http.createServer(requestHandler);
 
