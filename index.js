10217822b40b96ef2e2df448d1c1b53c8b3bf6f5
AshimBarman <ashlabbarman@gmail.com>
2023-09-02 01:01:46 +0530

    Server different html file in diffrent url

--- index.js
+++ index.js
@@ -6,13 +6,26 @@ const fs = require('fs');//use to reading or writting in file
 function requestHandler(req, res) {
     console.log("hello jee kaise ho saare");
     res.writeHead(200,{'context-type': 'text/html'});
-    fs.readFile('index.html', function (err,data) {
+    
+    let filePath;
+    switch(req.url) {
+        case '/':
+            filePath = './index.html';
+            break;
+        case '/profile':
+            filePath = './profile.html';
+            break;
+        default:
+            filePath = './404.html'        
+    }
+
+    fs.readFile(filePath, function(err, data) {
         if(err) {
-            console.log(err);
-            return res.end('<h1>error<h1>');
+            console.log('error', err);
+            return res.end('<h1>Error!</h1>');
         }
         return res.end(data);
-    });
+    })
 }
 const server = http.createServer(requestHandler);
 
